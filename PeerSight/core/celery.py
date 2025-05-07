897bbd1967f3de6b459344a4e0771994b9e5ec9d
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2025-05-07 04:51:01 -0400

    Update celery.py

--- PeerSight/core/celery.py
+++ PeerSight/core/celery.py
@@ -3,8 +3,12 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = Celery('PeerSight')
+app = Celery('peersight', broker=os.environ.get("CELERY_BROKER_URL"), backend=os.environ.get("CELERY_RESULT_BACKEND"))
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
+# Apply SSL cert option if using rediss://
+if app.conf.result_backend and app.conf.result_backend.startswith("rediss://"):
+    app.conf.update(
+        redis_backend_transport_options={'ssl_cert_reqs': 'CERT_NONE'}
+    )
 app.autodiscover_tasks()
