241c0a09e942c6e7d907ac717e68570743884799
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2025-05-07 05:05:50 -0400

    Update celery.py and settings.py

--- PeerSight/core/celery.py
+++ PeerSight/core/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = Celery('peersight', broker=os.environ.get("CELERY_BROKER_URL"), backend=os.environ.get("CELERY_RESULT_BACKEND"))
+app = Celery('peersight', broker=os.environ.get("CELERY_BROKER_URL"))
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
