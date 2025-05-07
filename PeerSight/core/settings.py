6a31573b79a1f5ae90a250db3d422814bf1481d7
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2025-05-06 22:17:54 -0400

    added superuser.json

--- PeerSight/core/settings.py
+++ PeerSight/core/settings.py
@@ -121,7 +121,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
